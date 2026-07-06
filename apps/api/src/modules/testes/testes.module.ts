76c191be3fd2f7fa7f9f05f3bcc585f5160508b0
Jarvis <jarvis@lofaegis.com>
2026-07-06 15:34:05 -0400

    fix: TestesModule must import BillingModule for ConsumoService DI

--- apps/api/src/modules/testes/testes.module.ts
+++ apps/api/src/modules/testes/testes.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BillingModule } from '../../billing/billing.module';
 import { TestesController } from './testes.controller';
 import { SessoesController } from './sessoes.controller';
 import { TestesService } from './testes.service';
 import { SessoesService } from './sessoes.service';
 
 @Module({
+  imports: [BillingModule],
   controllers: [TestesController, SessoesController],
   providers: [TestesService, SessoesService],
   exports: [TestesService, SessoesService],
